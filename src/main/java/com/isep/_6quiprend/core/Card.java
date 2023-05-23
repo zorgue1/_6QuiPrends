63c3fb9173044a7fff14f00f91e8f63bdf38a0ee
zorgue1 <116544814+zorgue1@users.noreply.github.com>
2023-05-23 12:52:25 +0200

    commencement

--- src/main/java/com/isep/_6quiprend/core/Card.java
+++ src/main/java/com/isep/_6quiprend/core/Card.java
@@ -1,4 +1,45 @@
 package com.isep._6quiprend.core;
 
 public class Card {
+
+    private int number ;
+    private int beefHead ;
+
+    public Card(int number) {
+        this.number = number;
+        setBeefHead(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getBeefHead() {
+        return beefHead;
+    }
+
+    public void setBeefHead(int number) {
+
+        int beefHead = 0 ;
+
+
+        if (number % 10 == 5) {
+            beefHead += 2;
+        } else if (number % 10 == 0) {
+            beefHead += 3;
+        } else if (number == 11 || number == 22) {
+            beefHead += 5;
+        } else {
+            beefHead = 1;
+        }
+
+        this.beefHead = beefHead ;
+
+    }
 }
+
+
--- src/main/java/com/isep/_6quiprend/core/Deck.java
+++ src/main/java/com/isep/_6quiprend/core/Deck.java
@@ -1,4 +1,28 @@
 package com.isep._6quiprend.core;
 
+import java.util.List;
+import java.util.Random;
+
 public class Deck {
+
+    private List<Card> deck;
+
+    public Deck(List<Card> deck) {
+        this.deck = deck;
+    }
+
+    public List<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(List<Card> deck) {
+        this.deck = deck;
+    }
+
+    public Card assignCard(){
+        Random random = new Random();
+        int number = random.nextInt(1,104);
+        Card card = new Card(number);
+        return card;
+    }
 }
