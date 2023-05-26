87e253248b0d0445bf65b12df3c74c34d13ead03
zorgue1 <116544814+zorgue1@users.noreply.github.com>
2023-05-26 16:58:05 +0200

    mise en place du tapis

--- src/main/java/com/example/_6quiprend/HelloApplication.java
+++ src/main/java/com/example/_6quiprend/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example._6quiprend;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,9 +12,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("6 Qui Prend !");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
